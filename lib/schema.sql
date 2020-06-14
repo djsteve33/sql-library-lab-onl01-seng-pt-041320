356e71d13e727e12ee306e0f2a589f677a9d4d6a
Steve Womble <swomble@aol.com>
2020-06-14 21:20:01 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,4 +3,4 @@ CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
 CREATE TABLE characters (id INTEGER PRIMARY KEY, NAME text, MOTTO text, species_id INTEGER, author_id INTEGER);
-CREATE TABLE character_books (id (id INTEGER PRIMARY KEY, character_id INTEGER, book_id INTEGER);
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, character_id INTEGER, book_id INTEGER);
